d11bd45a01980a63783ca58eb6ca9c2df01dc764
kessius00 <kes.olling00@gmail.com>
2025-05-16 00:51:50 +0200

    make dropdown-menu functional

--- src/index.js
+++ src/index.js
@@ -7,4 +7,32 @@ console.log('Hello World!');
 // Select elements
 const dropdownBtn = document.querySelector('.dropbtn');
 const dropdownContent = document.querySelector('.dropdown-content');
-const dropdownLinks = document.querySelectorAll('.dropdown-content a');
+
+// Add event listeners to dropdown links
+dropdownBtn.addEventListener('mouseover', () => {
+    if (dropdownBtn.classList.contains('active')) {
+        return;
+    } else {
+        dropdownContent.classList.remove('hidden');
+        dropdownContent.classList.add('translucent');
+    }
+
+    dropdownBtn.addEventListener('mouseleave', () => {
+        if (dropdownBtn.classList.contains('active')) {
+            return;
+        } else {
+            dropdownContent.classList.add('hidden');
+            dropdownContent.classList.remove('translucent');
+        }
+    });
+});
+
+dropdownBtn.addEventListener('click', () => {
+    if (dropdownContent.classList.contains('translucent')) {
+        dropdownContent.classList.remove('translucent');
+        dropdownBtn.classList.add('active');
+    } else {
+        dropdownBtn.classList.remove('active');
+        dropdownContent.classList.add('hidden');
+    }
+});
