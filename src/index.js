8e2abe5a6686596e4fbcb99cf5e26bf3b02bce7d
kessius00 <kes.olling00@gmail.com>
2025-05-15 14:30:49 +0200

    make dropdown in HTML and some queryselectors in js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import "./styles/reset.css";
 import "./styles/styles.scss";
 
 console.log("Hello World!");
+
+// Select elements
+const dropdownBtn = document.querySelector('.dropbtn');
+const dropdownContent = document.querySelector('.dropdown-content');
+const dropdownLinks = document.querySelectorAll('.dropdown-content a');
