3f50f5c67b1235478aae4155dafa88b3915fea2c
kessius00 <kes.olling00@gmail.com>
2025-05-16 02:33:34 +0200

    Add the elementmaker in JS and add dropdown classes

--- src/index.js
+++ src/index.js
@@ -2,37 +2,93 @@
 import './styles/reset.css';
 import './styles/styles.scss';
 
-console.log('Hello World!');
-
-// Select elements
-const dropdownBtn = document.querySelector('.dropbtn');
-const dropdownContent = document.querySelector('.dropdown-content');
-
-// Add event listeners to dropdown links
-dropdownBtn.addEventListener('mouseover', () => {
-    if (dropdownBtn.classList.contains('active')) {
-        return;
-    } else {
-        dropdownContent.classList.remove('hidden');
-        dropdownContent.classList.add('translucent');
+class DropDownItem {
+    constructor(name, link) {
+        this.name = name;
+        this.link = link;
     }
+}
+class DropDownClass {
+    constructor(title) {
+        this.title = title;
+        this.items = [];
+        this.selectedItem = null;
+        this.addItem = function (name, link) {
+            const item = new DropDownItem(name, link);
+            // Check if item is already in the list
+            if (this.items.includes(item)) {
+                console.log('Item already exists');
+                return;
+            }
+            this.items.push(item);
+        };
+    }
+}
 
-    dropdownBtn.addEventListener('mouseleave', () => {
-        if (dropdownBtn.classList.contains('active')) {
+const dropDown = new DropDownClass('My Dropdown');
+dropDown.addItem('Item 1', 'https://google.com/');
+dropDown.addItem('Item 2', 'https://google.com/');
+dropDown.addItem('Item 3', 'https://google.com/');
+
+function makeDropDownElement(dropDown) {
+    const dropDownContainer = document.createElement('div');
+    dropDownContainer.classList.add('dropdown');
+
+    // Create the button
+    const dropDownBtn = document.createElement('button');
+    dropDownBtn.classList.add('dropbtn');
+    dropDownBtn.innerText = dropDown.title;
+
+    // Create the dropdown content
+    const dropDownContent = document.createElement('div');
+    dropDownContent.classList.add('dropdown-content', 'dd-hidden');
+
+    dropDown.items.forEach((item) => {
+        const itemElement = document.createElement('a');
+        itemElement.innerText = item.name;
+        itemElement.href = item.link;
+        itemElement.target = '_blank';
+        itemElement.classList.add('dropdown-item');
+        dropDownContent.appendChild(itemElement);
+    });
+
+    // Add event listeners to dropdown links
+    dropDownBtn.addEventListener('mouseover', () => {
+        if (dropDownBtn.classList.contains('dd-active')) {
             return;
         } else {
-            dropdownContent.classList.add('hidden');
-            dropdownContent.classList.remove('translucent');
+            dropDownContent.classList.remove('dd-hidden');
+            dropDownContent.classList.add('dd-translucent');
+        }
+
+        dropDownBtn.addEventListener('mouseleave', () => {
+            // if it hovers over the list, do not remove the list
+            // HEREEE
+
+            if (dropDownBtn.classList.contains('dd-active')) {
+                return;
+            } else {
+                dropDownContent.classList.add('dd-hidden');
+                dropDownContent.classList.remove('dd-translucent');
+            }
+        });
+    });
+
+    dropDownBtn.addEventListener('click', () => {
+        if (dropDownContent.classList.contains('dd-translucent')) {
+            dropDownContent.classList.remove('dd-translucent');
+            dropDownBtn.classList.add('dd-active');
+        } else {
+            dropDownBtn.classList.remove('dd-active');
+            dropDownContent.classList.add('dd-hidden');
         }
     });
-});
 
-dropdownBtn.addEventListener('click', () => {
-    if (dropdownContent.classList.contains('translucent')) {
-        dropdownContent.classList.remove('translucent');
-        dropdownBtn.classList.add('active');
-    } else {
-        dropdownBtn.classList.remove('active');
-        dropdownContent.classList.add('hidden');
-    }
-});
+    dropDownContainer.append(dropDownBtn, dropDownContent);
+    return dropDownContainer;
+}
+
+const dropdown1 = makeDropDownElement(dropDown);
+
+const content = document.querySelector('.content');
+content.appendChild(dropdown1);
